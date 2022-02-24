763ab8ed4b1cab76e573eebdbd8f99602ea46172
fulladdr <jeymemoon@gmail.com>
2022-02-24 16:41:43 +0900

    cache lab modified, 21/27

--- lab4/csim.c
+++ lab4/csim.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -17,13 +18,13 @@ struct _set{
 
 struct _cache{
   struct _set *sets;
-  size_t setcount;  
-  size_t linecount;
+  unsigned int setcount;  
+  unsigned int linecount;
 };
 
 
 struct _cache cache = {};
-void update(struct _set *set, size_t linenum);
+void update(struct _set *set, unsigned int linenum);
 
 int main(int argc, char *argv[]) {
 
@@ -31,7 +32,7 @@ int main(int argc, char *argv[]) {
 	bool VERBOSE = false;
 	int s, E, b;
 	char *t;
-	while ((opt = getopt(argc, argv, "s:E:b:t:"))!=-1){
+	while ((opt = getopt(argc, argv, "s:E:b:t:v"))!=-1){
 		switch (opt){
 			case 'v':
 				VERBOSE = true;
@@ -73,12 +74,7 @@ int main(int argc, char *argv[]) {
 	}
 	for (int i=0;i<cache.setcount;i++){
 		cache.sets[i].lines = calloc(sizeof(struct _line), cache.linecount);
-		if (!cache.sets[i].lines){
-			printf("calloc error\n");
-			return -1;
-		}
 	}
-
 	char line[32];
 	char *tokenoperation, *tokenaddress, *tokensize;
 	unsigned int hitcount=0, misscount=0, evictioncount=0;
@@ -101,62 +97,73 @@ int main(int argc, char *argv[]) {
 		setindex = ((address << (64 -s -b)) >> (64 - s));
 
 		struct _set *set = &cache.sets[setindex];
-		
-		bool HIT = false;
-		for (size_t i=0;i<cache.linecount;i++){
-			struct _line* line = &set->lines[i];
-			if (!(line->valid))
-				continue;
-			if (line->tag != tag)
-				continue;
-			HIT = true;
-			++hitcount;
-			update(set, i);
-			break;
-		}
-		
-		bool EVICTION = true;
-		if (!HIT){
-			++misscount;
-			for (size_t i=0;i<cache.linecount;i++){
-				struct _line *line = &set->lines[i];
-				if (line->valid)
+		unsigned int mcount = 0;
+		do{
+			mcount++;
+			bool HIT = false;
+			for (unsigned int i=0;i<cache.linecount;i++){
+				struct _line* line = &set->lines[i];
+				if (!(line->valid))
 					continue;
-				// empty line found
-				EVICTION = false;
-				line->valid = true;
-				line->tag = tag;
+				if (line->tag != tag)
+					continue;
+				HIT = true;
+				++hitcount;
+				if (VERBOSE)
+					printf("hit ");
 				update(set, i);
 				break;
 			}
-			if (EVICTION){
-				++evictioncount;
-				unsigned int lrucount = -1, lruline;
-				for (size_t i=0;i<cache.linecount;i++){
-					struct _line* line = &set->lines[i];
-					if (line->timestamp > lrucount){
-						lrucount = line->timestamp;
-						lruline = i;
-					}
+			
+			bool EVICTION = true;
+			if (!HIT){
+				++misscount;
+				if (VERBOSE)
+					printf("miss ");
+				for (unsigned int i=0;i<cache.linecount;i++){
+					struct _line *line = &set->lines[i];
+					if (line->valid)
+						continue;
+					// empty line found
+					EVICTION = false;
+					line->valid = true;
+					line->tag = tag;
+					update(set, i);
+					break;
 				}
-				struct _line *line = &set->lines[lruline];
-				line->valid = true;
-				line->tag = tag;
-				update(set, lruline);
+				if (EVICTION){
+					++evictioncount;
+					if (VERBOSE)
+						printf("evict ");
+					unsigned int lrucount = -1, lruline;
+					for (unsigned int i=0;i<cache.linecount;i++){
+						struct _line* line = &set->lines[i];
+						if ((line->timestamp) > lrucount){
+							lrucount = line->timestamp;
+							lruline = i;
+						}
+					}
+					struct _line *line = &set->lines[lruline];
+					line->valid = true;
+					line->tag = tag;
+					update(set, lruline);
+				}
 			}
-		}
+		} while(line[1] == 'M' && mcount <2);
+		if (VERBOSE)
+			printf("\n");
 	}
 	printSummary(hitcount, misscount, evictioncount);
 	
 	fclose(tracefile);
-	for (size_t i=0;i<cache.setcount;i++)
+	for (unsigned int i=0;i<cache.setcount;i++)
 		free(cache.sets[i].lines);
 	free(cache.sets);
 	return 0;
 }
 
-void update(struct _set *set, size_t linenum){
-	for (size_t i=0;i<cache.linecount;i++){
+void update(struct _set *set, unsigned int linenum){
+	for (unsigned int i=0;i<cache.linecount;i++){
 		struct _line *l = &set->lines[i];
 		if (i==linenum)
 			l->timestamp=0;
